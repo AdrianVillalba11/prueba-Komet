f0c7e151c32bb4daca09f194239791020616f1f9
Adrian <adrianvillalba70@gmail.com>
2023-04-20 19:52:29 -0500

    Primer commit mas config properties

---
+++ src/main/java/com/api/Crud/Komet/CrudKometApplication.java
@@ -0,0 +1,13 @@
+package com.api.Crud.Komet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudKometApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudKometApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Crud/Komet/CrudKometApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Crud.Komet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKometApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
